81fcbdb35a441bc85825fc947c916f33e8e2b3fe
waymao <my.waymao+git@gmail.com>
2025-12-28 06:24:54 -0500

    migrate to postgres

--- packages/backend/prisma/migrations/20251228042821_add_soft_delete/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "tasks" ADD COLUMN "deletedAt" DATETIME;
-
--- CreateIndex
-CREATE INDEX "tasks_deletedAt_idx" ON "tasks"("deletedAt");
--- packages/backend/prisma/migrations/20251228074637_add_calendar_event_composite_index/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateIndex
-CREATE INDEX "google_calendar_accounts_userId_idx" ON "google_calendar_accounts"("userId");
-
--- CreateIndex
-CREATE INDEX "google_calendar_events_accountId_endTime_idx" ON "google_calendar_events"("accountId", "endTime");
-
--- CreateIndex
-CREATE INDEX "google_calendar_events_accountId_startTime_endTime_idx" ON "google_calendar_events"("accountId", "startTime", "endTime");
